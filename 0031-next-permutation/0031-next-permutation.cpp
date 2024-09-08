a5bb2c226f332fa917848ea2d90519388c292ea5
Aman Pandey(Anirudh) <116947899+losercodes@users.noreply.github.com>
2024-09-09 00:21:06 +0530

    Time: 0 ms (100%), Space: 14.9 MB (52.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> s;
-
-        // Step 1: Push elements onto the stack
-        for (int num : nums) {
-            s.push(num);
+        int i = nums.size() - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (nums[j] <= nums[i]) --j;
+            swap(nums[i], nums[j]);
         }
-
-        // Step 2: Create a new vector to hold the next permutation
-        vector<int> nextPerm;
-
-        // Step 3: Find the next permutation
-        for (int i = n - 1; i >= 0; --i) {
-            if (!s.empty() && s.top() > nums[i]) {
-                // Find the first element in the stack that is greater than nums[i]
-                nextPerm.push_back(s.top());
-                s.pop();
-                break;
-            } else {
-                nextPerm.push_back(nums[i]);
-                s.pop();
-            }
-        }
-
-        // Step 4: Add remaining elements from the stack in reverse order
-        while (!s.empty()) {
-            nextPerm.push_back(s.top());
-            s.pop();
-        }
-
-        // Step 5: Reverse the order of the elements that were added to nextPerm
-        reverse(nextPerm.begin() + 1, nextPerm.end());
-
-        // Step 6: Update the original nums array
-        nums = nextPerm;
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
