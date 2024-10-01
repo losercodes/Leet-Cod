15da5f4cc161a38ffb5f52ada645194dd2cb7be8
KUMAR KUSH <115366430+KUMAR-KUSH@users.noreply.github.com>
2024-10-01 20:08:05 +0530

    Update 0027-remove-element.cpp

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& num, int val) {
+        // 'remove' shifts all elements that are not equal to 'val' to the beginning
+        // of the vector and returns an iterator to the new end of the array.
         auto it = remove(num.begin(), num.end(), val);
-        num.erase(it, nums.end());
+        
+        // Erase the remaining elements from the point returned by 'remove' to the end of the vector.
+        num.erase(it, num.end());
+        
+        // Return the new size of the vector after erasing the elements.
         return num.size();
     }
 };
