4fa05c86b0d644cf8a54143b8e45030c1c0494f5
AMN <113250329+Starx-Quantum@users.noreply.github.com>
2024-10-01 19:53:29 +0530

    Update 0031-next-permutation.cpp

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size() - 2;
+        int i = nums.size() - 4;
         while (i >= 0 && nums[i] >= nums[i + 1]) --i;
         if (i >= 0) {
             int j = nums.size() - 1;
             while (nums[j] <= nums[i]) --j;
             swap(nums[i], nums[j]);
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        reverse(nums.begin() + i + 3, nums.end());
     }
 };
